92c05ad8ece28b7c8a46d056cfd1bcb7731904f6
dubonfelipe <dubonfelipe95@gmail.com>
2022-05-03 14:05:33 -0600

    Update ComponentUtils.java

--- src/src/src/main/java/org/vaadin/example/Backend/Utils/ComponentUtils.java
+++ src/src/src/main/java/org/vaadin/example/Backend/Utils/ComponentUtils.java
@@ -1,48 +1,31 @@
-package org.vaadin.example.Backend.Utils;
-
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.textfield.IntegerField;
-import com.vaadin.flow.component.textfield.TextField;
-
-public class  ComponentUtils {
-    public static final Component getField(String fieldText, String name, Integer typeField, boolean isRequired){
-        FormLayout temp = new FormLayout();
-        if(typeField == 1){
-            TextField primerNombre = new TextField(fieldText);
-            primerNombre.setRequiredIndicatorVisible(isRequired);
-            primerNombre.setRequired(isRequired);
-            primerNombre.setId(name);
-            primerNombre.addValueChangeListener(event->{
-                primerNombre.setValue(event.getValue().toUpperCase());
-
+ public static Component getComponent(Columna columna){
+        Component component = null;
+        if(columna.getTipoColumna() == 1){
+            TextField textField = new TextField(columna.getNombreColumna());
+            textField.setRequired(columna.isObligatorio());
+            textField.setRequiredIndicatorVisible(columna.isObligatorio());
+            textField.addValueChangeListener(event->{
+               //textField.setValue(event.getValue().toUpperCase());
             });
-
-            temp.add(primerNombre);
-            temp.add(primerNombre, 2);
-            return primerNombre;
-        }else if(typeField == 2){
-            IntegerField telefono = new IntegerField(fieldText);
-            telefono.setMax(99999999);
-            telefono.setMin(10000000);
-            telefono.setId(name);
-            temp.add(telefono);
-            temp.add(telefono,1);
-            return telefono;
-        }else if(typeField == 3){
-            Checkbox checkbox = new Checkbox("Acepta Terminos");
-            checkbox.setId(name);
-            checkbox.setRequiredIndicatorVisible(isRequired);
-
-            temp.add(checkbox);
-            temp.add(checkbox,4);
-            return checkbox;
-        }else if(typeField == 4){
-            DatePicker date = new DatePicker("Fecha");
-            return date;
+            textField.setAutocapitalize(Autocapitalize.CHARACTERS);
+            textField.setMaxLength(columna.getMax());
+            component = textField;
+        }else if(columna.getTipoColumna() == 2){
+            IntegerField integerField = new IntegerField(columna.getNombreColumna());
+            integerField.setRequiredIndicatorVisible(columna.isObligatorio());
+            integerField.setMax(columna.getMax());
+            component = integerField;
+        }else if(columna.getTipoColumna() == 3){
+            NumberField numberField = new NumberField(columna.getNombreColumna());
+            numberField.setRequiredIndicatorVisible(columna.isObligatorio());
+            numberField.setStep(0.01);
+            numberField.setMax(columna.getMax());
+            component = numberField;
+        }else if(columna.getTipoColumna() == 4){
+            DatePicker datePicker = new DatePicker(columna.getNombreColumna());
+            datePicker.setRequired(columna.isObligatorio());
+            datePicker.setRequiredIndicatorVisible(columna.isObligatorio());
+            component = datePicker;
         }
-        return temp;
+        return component;
     }
-}
